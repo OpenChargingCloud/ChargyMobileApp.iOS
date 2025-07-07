001857cbf6f6a9b4c7979f3f9bf9fafc75c411c6
Achim Friedland <achim.friedland@graphdefined.com>
2025-07-08 00:12:35 +0200

    Initial Commit

---
+++ ChargyMobileApp.iOS/ChargyMobileApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChargyMobileApp_iOSApp.swift
+//  ChargyMobileApp.iOS
+//
+//  Created by Achim Friedland on 08.07.25.
+//
+
+import SwiftUI
+
+@main
+struct ChargyMobileApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChargyMobileApp.iOSTests/ChargyMobileApp_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChargyMobileApp_iOSTests.swift
+//  ChargyMobileApp.iOSTests
+//
+//  Created by Achim Friedland on 08.07.25.
+//
+
+import Testing
+@testable import ChargyMobileApp_iOS
+
+struct ChargyMobileApp_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
